5cfa10d81c049f0f49415fe7df491e3ebabe5895
unknown <bogdan.soucup@gmail.com>
2022-05-22 13:19:10 +0300

    Simple onthology parsing

--- src/extension.ts
+++ src/extension.ts
@@ -22,7 +22,7 @@ const legend = (function () {
 })();
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'semanticLanguage'}, new DocumentSemanticTokensProvider(), legend));
+	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'racer'}, new DocumentSemanticTokensProvider(), legend));
 }
 
 interface IParsedToken {
@@ -65,38 +65,92 @@ class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTokensPro
 		return result;
 	}
 
-	
+	private _findAllNewlines(text: string): number[] {
+		let newLines: number[] = [];
+		for(let i = 0; i < text.length; i++) {
+			if(text[i] === '\n') {
+				newLines.push(i);
+			}
+		}
+		return newLines;
+	}
+
+	private _findLine(index: number, newLines: number[]): number {
+		let currentIndex = 0;
+		while(currentIndex < newLines.length) {
+			if(currentIndex && newLines[currentIndex] > index) {
+				return newLines[currentIndex - 1];
+			}
+			currentIndex++;
+		}
+		return 0;
+	}
+
+	private _findLineIndex(index: number, newLines: number[]): number {
+		let currentIndex = 0;
+		while(currentIndex < newLines.length) {
+			if(currentIndex && newLines[currentIndex] > index) {
+				return currentIndex - 1;
+			}
+			currentIndex++;
+		}
+		return 0;
+	}
 
 	private _parseText(text: string): IParsedToken[] {
 		const r: IParsedToken[] = [];
-		console.log(pl.getSomething("Vasile"))
-		const lines = text.split(/\r\n|\r|\n/);
-		for (let i = 0; i < lines.length; i++) {
-			const line = lines[i];
-			let currentOffset = 0;
-			do {
-				const openOffset = line.indexOf('[', currentOffset);
-				if (openOffset === -1) {
-					break;
+		
+		let index = text.indexOf("implies");
+		let newLines = this._findAllNewlines(text);
+		while(index !== -1) {
+			let theNewLine = this._findLine(index, newLines);
+			let lineIndex = this._findLineIndex(index, newLines);
+
+			console.log(lineIndex + 1, index - theNewLine - 1)
+			r.push(
+				{
+					line: lineIndex + 1,
+					startCharacter: index - theNewLine - 1,
+					length: "implies".length,
+					tokenType: "string",
+					tokenModifiers: []
 				}
-				const closeOffset = line.indexOf(']', openOffset);
-				if (closeOffset === -1) {
-					break;
-				}
-				const tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset));
-				r.push({
-					line: i,
-					startCharacter: openOffset + 1,
-					length: closeOffset - openOffset - 1,
-					tokenType: tokenData.tokenType,
-					tokenModifiers: tokenData.tokenModifiers
-				});
-				console.log(i, openOffset + 1, closeOffset - openOffset - 1)
-				currentOffset = closeOffset;
-			} while (true);
+			);
+			index = text.indexOf("implies", index + 1)
 		}
 		return r;
 	}
+	
+
+	// private _parseText(text: string): IParsedToken[] {
+	// 	const r: IParsedToken[] = [];
+	// 	const lines = text.split(/\r\n|\r|\n/);
+	// 	console.log("Vasile")
+	// 	for (let i = 0; i < lines.length; i++) {
+	// 		const line = lines[i];
+	// 		let currentOffset = 0;
+	// 		do {
+	// 			const openOffset = line.indexOf('[', currentOffset);
+	// 			if (openOffset === -1) {
+	// 				break;
+	// 			}
+	// 			const closeOffset = line.indexOf(']', openOffset);
+	// 			if (closeOffset === -1) {
+	// 				break;
+	// 			}
+	// 			const tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset));
+	// 			r.push({
+	// 				line: i,
+	// 				startCharacter: openOffset + 1,
+	// 				length: closeOffset - openOffset - 1,
+	// 				tokenType: tokenData.tokenType,
+	// 				tokenModifiers: tokenData.tokenModifiers
+	// 			});
+	// 			currentOffset = closeOffset;
+	// 		} while (true);
+	// 	}
+	// 	return r;
+	// }
 
 	private _parseTextToken(text: string): { tokenType: string; tokenModifiers: string[]; } {
 		const parts = text.split('.');
